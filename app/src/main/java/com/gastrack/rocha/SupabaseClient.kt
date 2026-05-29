0afa85ad0f8c7fbc8e5fc3160c7a338c48f34387
MEGALROCHA <rochamegal5@gmail.com>
2026-05-29 17:34:36 -0300

    Update SupabaseClient.kt

--- app/src/main/java/com/gastrack/rocha/SupabaseClient.kt
+++ app/src/main/java/com/gastrack/rocha/SupabaseClient.kt
@@ -6,7 +6,7 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClient {
     // ⚠️ REEMPLAZAR CON TUS DATOS DE SUPABASE ⚠️
-    private const val SUPABASE_URL = "https://plnodukthcnnwmxlzwsm.supabase.co/rest/v1/"
+    private const val SUPABASE_URL = "https://plnodukthcnnwmxlzwsm.supabase.co"
     private const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsbm9kdWt0aGNubndteGx6d3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNTkwMTcsImV4cCI6MjA5NTYzNTAxN30.WkTzFvNiM0BotwTLtxzQpq9-b08stueIlsp8K0b9iq8"
 
     val client: SupabaseClient by lazy {
